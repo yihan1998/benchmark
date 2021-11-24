b2b1a978026faa633df1988854e78420c057d1e7
Yang Yihan <87106800+yihan1998@users.noreply.github.com>
2021-11-24 16:15:27 +0800

    Update redis_db.cc

--- YCSB_redis_client/db/redis_db.cc
+++ YCSB_redis_client/db/redis_db.cc
@@ -33,8 +33,8 @@ int RedisDB::Scan(const std::string &table, const std::string &key, int record_c
   int index;
   int read = 0;
   do {
-    redisReply *reply = (redisReply *)redisCommand(redis_.context(), "SCAN %d COUNT %d", index, record_count - read);
-    // redisReply *reply = (redisReply *)redisCommand(redis_.context(), "SCAN %d", 0);
+    // redisReply *reply = (redisReply *)redisCommand(redis_.context(), "SCAN %d COUNT %d", index, record_count - read);
+    redisReply *reply = (redisReply *)redisCommand(redis_.context(), "SCAN %d", index);
     if (!reply) return DB::kOK;
     index = stoi(reply->element[0]->str);
     printf("index: %d\n",index);
@@ -47,11 +47,10 @@ int RedisDB::Scan(const std::string &table, const std::string &key, int record_c
       freeReplyObject(reply);
       return DB::kErrorNoData;
     }
-    read += reply->element[1]->elements;
+    // read += reply->element[1]->elements;
     printf("elements: %d\n",reply->element[1]->elements);
     for (int i = 0; i < reply->element[1]->elements; i++) {
-      string key = reply->element[1]->element[i]->str;
-      printf("i: %d,key: %s\n",i,key);
+      printf("key: %s\n", reply->element[1]->element[i]->str);
     }
   } while (index != 0);
 
