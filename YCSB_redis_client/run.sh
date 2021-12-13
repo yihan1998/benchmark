f075dd63bbf6aa727d89ba17e6bffae613c7fd3f
Yang Yihan <yangyihan1998@gmail.com>
2021-12-13 19:59:39 +0800

    udpate

--- YCSB_redis_client/run.sh
+++ YCSB_redis_client/run.sh
@@ -1,7 +1,7 @@
 trap 'kill $(jobs -p)' SIGINT
 
 for file_name in ./workloads/workload*.spec; do
-    for ((tn=1; tn<=8; tn=tn*2)); do
+    for ((tn=1; tn<=1; tn=tn*2)); do
         echo "Running with $tn threads for $file_name"
         ./client --port=6379 --flows=1 --num_cores=$tn --workload=$file_name 2>>ycsbc.output &
         wait
